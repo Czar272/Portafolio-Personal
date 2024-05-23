879f7960843e2c6d894849de1ecf508df2d2fd01
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-05-23 17:36:53 -0600

    Eleccion de colores

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -5,14 +5,19 @@ function App() {
   return (
     <>
       <div className="contenedor">
-        <div className="header"></div>
-        <div className="Title">Designer & Frontend Developer</div>
-        <div className="subtitles">
-          <div className="btn">Studys</div>
-          <div className="btn">Projects</div>
-          <div className="btn">Experience</div>
-          <div className="btn">Contact Me</div>
-        </div>
+        {/* <div className="header">
+          <div className="leftHeader"></div>
+          <div className="rightHeader"></div>
+        </div> */}
+        {/* <div className="content"> */}
+          <div className="Title">Designer Y Frontend Developer</div>
+          <div className="subtitles">
+            <div className="btn">Studys</div>
+            <div className="btn">Projects</div>
+            <div className="btn">Experience</div>
+            <div className="btn">Contact Me</div>
+          </div>
+        {/* </div> */}
       </div>
     </>
   )
