f6306ae807291cc4d62746b8051020b490f5f27d
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-05-23 12:12:00 -0600

    Estructura

---
+++ Portafolio/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="contenedor">
+        <div className="header"></div>
+        <div className="Title">Designer & Frontend Developer</div>
+        <div className="subtitles">
+          <div className="btn">Studys</div>
+          <div className="btn">Projects</div>
+          <div className="btn">Experience</div>
+          <div className="btn">Contact Me</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
