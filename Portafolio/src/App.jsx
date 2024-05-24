488f8dde5d0ce81f83b62d7aae7659dab18901fc
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-05-23 20:06:52 -0600

    Scroll content

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
             <div className="imgMenu"></div>
           </div>
         </div>
-        {/* <div className="content"> */}
+        <div className="content">
           <div className="Title">Designer Y Frontend Developer</div>
           <div className="subtitles">
             <div className="btn">Studies</div>
@@ -21,11 +21,11 @@ function App() {
             <div className="btn">Experience</div>
             <div className="btn">Contact Me</div>
           </div>
-          <div className="Studies"></div>
+          <div className="Studies"> Studies</div>
           <div className="Projects"></div>
           <div className="Experience"></div>
           <div className="Contact"></div>
-        {/* </div> */}
+        </div>
       </div>
     </>
   )
