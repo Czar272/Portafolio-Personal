f7ee98d3f26eecb08072194eeff88a555f5f3e95
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-05-23 19:19:58 -0600

    Redisenio

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -5,18 +5,26 @@ function App() {
   return (
     <>
       <div className="contenedor">
-        {/* <div className="header">
-          <div className="leftHeader"></div>
-          <div className="rightHeader"></div>
-        </div> */}
+        <div className="header">
+          <div className="leftHeader">
+            <div className="profilePic"> César López </div>
+          </div>
+          <div className="rightHeader">
+            <div className="imgMenu"></div>
+          </div>
+        </div>
         {/* <div className="content"> */}
           <div className="Title">Designer Y Frontend Developer</div>
           <div className="subtitles">
-            <div className="btn">Studys</div>
+            <div className="btn">Studies</div>
             <div className="btn">Projects</div>
             <div className="btn">Experience</div>
             <div className="btn">Contact Me</div>
           </div>
+          <div className="Studies"></div>
+          <div className="Projects"></div>
+          <div className="Experience"></div>
+          <div className="Contact"></div>
         {/* </div> */}
       </div>
     </>
