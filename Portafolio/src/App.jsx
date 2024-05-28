8e95224a19cbf3a09a60c868981c38eeec7035ec
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-05-28 16:23:42 -0600

    Estructura completa

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
 
+  // external js: flickity.pkgd.js
+
   const [showSustBtn, setShowSustBtn] = useState(false)
 
   useEffect(() => {
@@ -31,13 +33,17 @@ function App() {
       <div className="contenedor">
         <div className="header">
           <div className="leftHeader">
-            <div className="profilePic"> César López </div>
+            <div className="profilePic">
+              <div className="luna">
+                <div className="lunaimg"></div>
+              </div>
+            </div>
           </div>
           <div id = "midHeader" className="midHeader">
-              <div className="sustBtn">Studies</div>
+              {/* <div className="sustBtn">Studies</div>
               <div className="sustBtn">Projects</div>
               <div className="sustBtn">Experience</div>
-              <div className="sustBtn">Contact Me</div>
+              <div className="sustBtn">Contact Me</div> */}
           </div>
           <div className="rightHeader">
             <div className="imgMenu"></div>
@@ -69,22 +75,21 @@ function App() {
               </a>
             </div>
           </div>
-          <div className="Categoria"> Studies:
+          <div className="Categoria" style={{backgroundColor: '#348888', color: 'white', border: '3px solid black'}}> Studies:
             <div className="CategoriaInfo">
-              <div className="StudyD">
-                <div className="PicnName">
+              <div className="StudyD" style={{backgroundColor: 'white', border: '3px solid black'}}>
+                <div className="PicnName" >
                   <div className="pic1"></div>
                   <div className="name">Bilingue el Prado</div>
                 </div>
                 <div className="StudyDesc"><br/> <b>Bilingue el Prado</b>   Estudie en este colegio desde el 7mo grado hasta el 12vo, habiéndome graduado de ahi sali con un bachiller en ciencias y letras con orientación en computación.  </div>
               </div>
-              <a className="StudyD" href='https://www.uvg.edu.gt'>
+              <a className="StudyD " style={{backgroundColor: 'white', border: '3px solid black'}} href='https://www.uvg.edu.gt'>
                 <div className="PicnName">
                     <div className="pic2"></div>
                     <div className="name">UVG</div>
                   </div>
                 <div className="StudyDesc"><br/> <b>Universidad del Valle de Guatemala</b> Actualmente cursando el 6to semestre o 3er año, todos los cursos aprobados y con promedio anual de 80 en adelante. </div>
- 
               </a>
             </div>
           </div>
@@ -108,7 +113,7 @@ function App() {
             </div>
             <div className="CardsB">
               <div className="card">
-                <div className="imgCard"></div>
+                <div className="imgCard" style={{backgroundImage: 'url(../src/assets/imagenes/BlogPic2.jpg)'}}></div>
                 <div className="TitleCard"> Blog</div>
                 <div className="descCard">  Blog de la historia de un video juego, todo su contenido es extraído de una base de datos realizada en MySQL workbench.</div>
               </div>
@@ -118,13 +123,35 @@ function App() {
                 <div className="descCard"> <br/>Una pagina web que despliega un dibujo solamente hecho con CSS, siendo interactivo y con animaciones</div>
               </a>              
               <div className="card">
-                <div className="imgCard"></div>
+                <div className="imgCard" style={{backgroundImage: 'url(../src/assets/imagenes/editimg1.jpg)'}}></div>
                 <div className="TitleCard"> Editar Post</div>
                 <div className="descCard">  Una funcionalidad para el blog, donde modifica una base de datos por medio de querys desde la misma pagina web. </div>
               </div>
             </div>
           </div>
-          <div className="Experience"></div>
+          <div className="Categoria" style={{backgroundColor: '#348888', color: 'white', border: '3px solid black'}}>
+            Tecnlogies:
+            <div className="tecnologias">
+              <div className="rowTec">
+                <div className="tecImgs" style={{backgroundImage: 'url(https://w1.pngwing.com/pngs/835/530/png-transparent-python-logo-programming-language-computer-programming-python-programming-basics-for-absolute-beginners-scripting-language-source-code-php-code-climate-inc-thumbnail.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://cdn-icons-png.flaticon.com/512/226/226777.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://ayudawp.com/wp-content/uploads/2017/01/javascript-logo-escudo.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://miro.medium.com/v2/resize:fit:1144/0*9je4S8adxijCGILV.jpg)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://cdn-icons-png.flaticon.com/256/174/174854.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://img-resize-cdn.joshmartin.ch/768x0%2Cc3537b9f46b5f6055fbc8b4cd03b6b2cc63fc2eefd3d8cd9f0c9f99a5933e496/https://joshmartin.ch/app/uploads/2017/10/css3.svg)'}}></div>
+              </div>
+              <div className="rowTec">
+                <div className="tecImgs" style={{backgroundImage: 'url(https://www.step2gen.com/WebsiteAssets/assets/images/nodejs.svg)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://javguerra.github.io/assets/img/mysql.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://www.docker.com/wp-content/uploads/2023/05/symbol_blue-docker-logo.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://avatars.githubusercontent.com/u/18133?s=280&v=4)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png )'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1200px-Amazon_Web_Services_Logo.svg.png)'}}></div>
+                <div className="tecImgs" style={{backgroundImage: 'url(https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png)'}}></div>
+              </div>              
+            </div>
+          </div>
           <div className="Contact"></div>
         </div>
       </div>
